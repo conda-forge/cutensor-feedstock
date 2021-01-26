760b8805500674fca86660cef146f88ab8496ee4
Leo Fang <leofang@bnl.gov>
2021-01-26 00:40:52 -0500

    debug

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -4,7 +4,7 @@ set -ex
 
 git clone https://github.com/NVIDIA/CUDALibrarySamples.git sample_linux/
 cd sample_linux/cuTENSOR/
-error_log=$(nvcc -I$PREFIX/include -L$PREFIX/lib -lcutensor contraction.cu -o contraction 2>&1)
+error_log=$(nvcc -v -I$PREFIX/include -L$PREFIX/lib -lcutensor contraction.cu -o contraction 2>&1)
 echo $error_log
-error_log=$(nvcc -I$PREFIX/include -L$PREFIX/lib -lcutensor reduction.cu -o reduction 2>&1)
+error_log=$(nvcc -v -I$PREFIX/include -L$PREFIX/lib -lcutensor reduction.cu -o reduction 2>&1)
 echo $error_log
