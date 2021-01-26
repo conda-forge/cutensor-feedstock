72561b730768c0d5b55f71230f0266913a353836
Leo Fang <leofang@bnl.gov>
2021-01-26 00:13:20 -0500

    debug

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,7 +1,14 @@
 @echo on
+setlocal EnableDelayedExpansion
 
 
 git clone "https://github.com/NVIDIA/CUDALibrarySamples.git" sample_linux
+if errorlevel 1 exit 1
+
 cd sample_linux\cuTENSOR
-nvcc -I$PREFIX\include -L$PREFIX\lib -lcutensor contraction.cu -o contraction
-nvcc -I$PREFIX\include -L$PREFIX\lib -lcutensor reduction.cu -o reduction
+
+nvcc -I%LIBRARY_INC% -L%LIBRARY_LIB% -lcutensor contraction.cu -o contraction
+if errorlevel 1 exit 1
+
+nvcc -I%LIBRARY_INC% -L%LIBRARY_LIB% -lcutensor reduction.cu -o reduction
+if errorlevel 1 exit 1
--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -4,5 +4,9 @@ set -ex
 
 git clone https://github.com/NVIDIA/CUDALibrarySamples.git sample_linux/
 cd sample_linux/cuTENSOR/
+which nvcc
+ls -l .
+ls -l $PREFIX/include
+ls -l $PREFIX/lib
 nvcc -I$PREFIX/include -L$PREFIX/lib -lcutensor contraction.cu -o contraction
 nvcc -I$PREFIX/include -L$PREFIX/lib -lcutensor reduction.cu -o reduction
