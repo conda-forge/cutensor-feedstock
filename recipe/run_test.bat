3fbb10934615c1af4b32de5da66e5abdebc6c50c
Leo Fang <leofang@bnl.gov>
2021-01-25 23:43:49 -0500

    fix

---
+++ recipe/run_test.bat
@@ -0,0 +1,7 @@
+@echo on
+
+
+git clone "https://github.com/NVIDIA/CUDALibrarySamples.git" sample_linux
+cd sample_linux\cuTENSOR
+nvcc -I$PREFIX\include -L$PREFIX\lib -lcutensor contraction.cu -o contraction
+nvcc -I$PREFIX\include -L$PREFIX\lib -lcutensor reduction.cu -o reduction
--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -4,5 +4,5 @@ set -ex
 
 git clone https://github.com/NVIDIA/CUDALibrarySamples.git sample_linux/
 cd sample_linux/cuTENSOR/
-nvcc contraction.cu -o contraction
-nvcc reduction.cu -o reduction
+nvcc -I$PREFIX/include -L$PREFIX/lib -lcutensor contraction.cu -o contraction
+nvcc -I$PREFIX/include -L$PREFIX/lib -lcutensor reduction.cu -o reduction
