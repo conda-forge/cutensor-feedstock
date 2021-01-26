e0984fd89d0aff01ba870628634117709e113f1c
Leo Fang <leofang@bnl.gov>
2021-01-26 01:58:07 -0500

    add call to win scripts

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -3,12 +3,6 @@ setlocal EnableDelayedExpansion
 
 
 git clone "https://github.com/NVIDIA/CUDALibrarySamples.git" sample_linux
-if errorlevel 1 exit 1
-
 cd sample_linux\cuTENSOR
-
-nvcc -I%LIBRARY_INC% -L%LIBRARY_LIB% -lcutensor contraction.cu -o contraction
-if errorlevel 1 exit 1
-
-nvcc -I%LIBRARY_INC% -L%LIBRARY_LIB% -lcutensor reduction.cu -o reduction
-if errorlevel 1 exit 1
+call nvcc -I%LIBRARY_INC% -L%LIBRARY_LIB% -lcutensor contraction.cu -o contraction
+call nvcc -I%LIBRARY_INC% -L%LIBRARY_LIB% -lcutensor reduction.cu -o reduction
