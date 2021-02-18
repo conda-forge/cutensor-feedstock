a1b534a9347e45b0ccefd1a7e744b0fb962f570e
Leo Fang <leofang@bnl.gov>
2021-02-18 12:06:33 -0500

    try a dumb test

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -2,6 +2,11 @@
 set -ex
 
 
+if [ $cuda_compiler_version == '11.0' ]; then
+    conda create -n test cudatoolkit=11.2 cutensor
+fi
+
+
 git clone https://github.com/NVIDIA/CUDALibrarySamples.git sample_linux/
 cd sample_linux/cuTENSOR/
 error_log=$(nvcc -I$PREFIX/include -L$PREFIX/lib -lcutensor contraction.cu -o contraction 2>&1)
