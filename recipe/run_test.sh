d7354c266756ffbb864f362294aef1892cacd6ef
Leo Fang <leofang@bnl.gov>
2021-02-18 19:32:31 -0500

    remove dumb test

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -2,11 +2,6 @@
 set -ex
 
 
-if [ $cuda_compiler_version == '11.0' ]; then
-    conda create -n test cudatoolkit=11.1 cutensor
-fi
-
-
 git clone https://github.com/NVIDIA/CUDALibrarySamples.git sample_linux/
 cd sample_linux/cuTENSOR/
 error_log=$(nvcc -I$PREFIX/include -L$PREFIX/lib -lcutensor contraction.cu -o contraction 2>&1)
