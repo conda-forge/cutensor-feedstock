bb426976a03d8f5880102263651c3f91bc621a4f
Leo Fang <leo80042@gmail.com>
2021-08-11 12:31:00 -0400

    HACK: make CUDA runtime symbols available

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -4,7 +4,7 @@ set -ex
 test -f $PREFIX/include/cutensor.h
 test -f $PREFIX/include/cutensor/types.h
 test -f $PREFIX/lib/libcutensor.so
-${GCC} test_load_elf.c -std=c99 -Werror -ldl -o test_load_elf
+${GCC} test_load_elf.c -std=c99 -Werror -ldl -lcudart -o test_load_elf
 ./test_load_elf $PREFIX/lib/libcutensor.so
 
 NVCC_FLAGS=""
