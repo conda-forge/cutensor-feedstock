ce884c186688a536322a793b2e053fd111366e9c
Leo Fang <leof@nvidia.com>
2023-04-04 17:33:26 -0400

    Update run_test.bat

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -9,9 +9,9 @@ if not exist %LIBRARY_BIN%\\cutensorMg.dll exit 1
 if not exist %LIBRARY_LIB%\\cutensor.lib exit 1
 if not exist %LIBRARY_LIB%\\cutensorMg.lib exit 1
 
-git clone "https://github.com/NVIDIA/CUDALibrarySamples.git" sample_linux
-cd sample_linux\cuTENSOR
-call nvcc -I%LIBRARY_INC% -L%LIBRARY_LIB% -lcutensor contraction.cu -o contraction
-call nvcc -I%LIBRARY_INC% -L%LIBRARY_LIB% -lcutensor reduction.cu -o reduction
-cd ..\cuTENSORMg
-call nvcc -I%LIBRARY_INC% -L%LIBRARY_LIB% -lcutensorMg -lcutensor contraction_multi_gpu.cu -o contraction_multi_gpu
+#git clone "https://github.com/NVIDIA/CUDALibrarySamples.git" sample_linux
+#cd sample_linux\cuTENSOR
+#call nvcc -I%LIBRARY_INC% -L%LIBRARY_LIB% -lcutensor contraction.cu -o contraction
+#call nvcc -I%LIBRARY_INC% -L%LIBRARY_LIB% -lcutensor reduction.cu -o reduction
+#cd ..\cuTENSORMg
+#call nvcc -I%LIBRARY_INC% -L%LIBRARY_LIB% -lcutensorMg -lcutensor contraction_multi_gpu.cu -o contraction_multi_gpu
