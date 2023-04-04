09632be22e72d66aa3a9021d43a2f4e1d8ee181c
Leo Fang <leof@nvidia.com>
2023-04-04 17:33:07 -0400

    Update run_test.sh

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -10,18 +10,18 @@ ${GCC} test_load_elf.c -std=c99 -Werror -ldl -o test_load_elf
 ./test_load_elf $PREFIX/lib/libcutensor.so
 ./test_load_elf $PREFIX/lib/libcutensorMg.so
 
-NVCC_FLAGS=""
-# Workaround __ieee128 error; see https://github.com/LLNL/blt/issues/341
-if [[ $target_platform == linux-ppc64le && $cuda_compiler_version == 10.* ]]; then
-    NVCC_FLAGS+=" -Xcompiler -mno-float128"
-fi
-
-git clone https://github.com/NVIDIA/CUDALibrarySamples.git sample_linux/
-cd sample_linux/cuTENSOR/
-error_log=$(nvcc $NVCC_FLAGS --std=c++11 -I$PREFIX/include -L$PREFIX/lib -lcutensor -lcudart contraction.cu -o contraction 2>&1)
-echo $error_log
-error_log=$(nvcc $NVCC_FLAGS --std=c++11 -I$PREFIX/include -L$PREFIX/lib -lcutensor -lcudart reduction.cu -o reduction 2>&1)
-echo $error_log
-cd ../cuTENSORMg/
-error_log=$(nvcc $NVCC_FLAGS --std=c++11 -I$PREFIX/include -L$PREFIX/lib -lcutensorMg -lcutensor -lcudart contraction_multi_gpu.cu -o contraction_multi_gpu 2>&1)
-echo $error_log
+#NVCC_FLAGS=""
+## Workaround __ieee128 error; see https://github.com/LLNL/blt/issues/341
+#if [[ $target_platform == linux-ppc64le && $cuda_compiler_version == 10.* ]]; then
+#    NVCC_FLAGS+=" -Xcompiler -mno-float128"
+#fi
+#
+#git clone https://github.com/NVIDIA/CUDALibrarySamples.git sample_linux/
+#cd sample_linux/cuTENSOR/
+#error_log=$(nvcc $NVCC_FLAGS --std=c++11 -I$PREFIX/include -L$PREFIX/lib -lcutensor -lcudart contraction.cu -o contraction 2>&1)
+#echo $error_log
+#error_log=$(nvcc $NVCC_FLAGS --std=c++11 -I$PREFIX/include -L$PREFIX/lib -lcutensor -lcudart reduction.cu -o reduction 2>&1)
+#echo $error_log
+#cd ../cuTENSORMg/
+#error_log=$(nvcc $NVCC_FLAGS --std=c++11 -I$PREFIX/include -L$PREFIX/lib -lcutensorMg -lcutensor -lcudart contraction_multi_gpu.cu -o contraction_multi_gpu 2>&1)
+#echo $error_log
