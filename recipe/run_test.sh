caf85c5073f1281b025e36ecc787c630502c8299
Matthew Jones <13078940+mtjrider@users.noreply.github.com>
2024-07-31 17:21:20 -0600

    increased logging for compilation tests

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -31,12 +31,44 @@ if [[ $target_platform == linux-ppc64le && $cuda_compiler_version == 10.* ]]; th
     NVCC_FLAGS+=" -Xcompiler -mno-float128"
 fi
 
+compile() {
+  local source=${1}
+  local output=${2}
+  local link_libraries=${3}
+  local _stdout _stderr
+  
+  _stdout=$(mktemp)
+  _stderr=$(mktemp)
+  
+  nvcc ${NVCC_FLAGS} --std=c++11 \
+    -I${PREFIX}/include \
+    -L${PREFIX}/lib ${link_libraries} \
+    ${source} --output-file $output \
+  > "${_stdout}" 2> "${_stderr}"
+  local _exit=$?
+
+  if [ ${_exit} -ne 0 ]; then
+    echo "nonzero exit code: ${_exit}"
+    echo "error compiling ${source}"
+    cat "${_stderr}"
+  else
+    echo "zero exit code: ${_exit}"
+    echo "successfully compiled ${source}"
+    cat "${_stdout}"
+  fi
+
+  rm -f "${_stdout}" "${_stderr}"
+}
+
 git clone https://github.com/NVIDIA/CUDALibrarySamples.git sample_linux/
 cd sample_linux/cuTENSOR/
-error_log=$(nvcc $NVCC_FLAGS --std=c++11 -I$PREFIX/include -L$PREFIX/lib -lcutensor -lcudart contraction.cu -o contraction 2>&1)
-echo $error_log
-error_log=$(nvcc $NVCC_FLAGS --std=c++11 -I$PREFIX/include -L$PREFIX/lib -lcutensor -lcudart reduction.cu -o reduction 2>&1)
-echo $error_log
+compile "contraction.cu" "contraction" "-lcutensor -lcudart"
+# error_log=$(nvcc $NVCC_FLAGS --std=c++11 -I$PREFIX/include -L$PREFIX/lib -lcutensor -lcudart contraction.cu -o contraction 2>&1)
+# echo $error_log
+compile "reduction.cu" "reduction" "-lcutensor -lcudart"
+# error_log=$(nvcc $NVCC_FLAGS --std=c++11 -I$PREFIX/include -L$PREFIX/lib -lcutensor -lcudart reduction.cu -o reduction 2>&1)
+# echo $error_log
 cd ../cuTENSORMg/
-error_log=$(nvcc $NVCC_FLAGS --std=c++11 -I$PREFIX/include -L$PREFIX/lib -lcutensorMg -lcutensor -lcudart contraction_multi_gpu.cu -o contraction_multi_gpu 2>&1)
-echo $error_log
+compile "contraction_multi_gpu.cu" "contraction_multi_gpu" "-lcutensorMg -lcutensor -lcudart"
+# error_log=$(nvcc $NVCC_FLAGS --std=c++11 -I$PREFIX/include -L$PREFIX/lib -lcutensorMg -lcutensor -lcudart contraction_multi_gpu.cu -o contraction_multi_gpu 2>&1)
+# echo $error_log
