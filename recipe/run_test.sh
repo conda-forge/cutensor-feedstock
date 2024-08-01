cc2c798a5d54cdc53fe13a24d38fe64ae4b058b8
Matthew Jones <13078940+mtjrider@users.noreply.github.com>
2024-08-01 16:15:49 -0600

    ... adding dropped link libraries

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -27,7 +27,7 @@ if [[ $target_platform == "linux-64" ]]; then
     cd sample_linux/cuTENSOR/
     error_log=$( nvcc --verbose ${NVCC_FLAGS} --std=c++11 -I${PREFIX}/include -L${PREFIX}/lib -lcutensor -lcudart contraction.cu -o contraction 2>&1 )
     echo "${error_log}"
-    error_log=$( nvcc --verbose ${NVCC_FLAGS} --std=c++11 -I${PREFIX}/include -L${PREFIX}/lib reduction.cu -o reduction 2>&1 )
+    error_log=$( nvcc --verbose ${NVCC_FLAGS} --std=c++11 -I${PREFIX}/include -L${PREFIX}/lib -lcutensor -lcudart reduction.cu -o reduction 2>&1 )
     echo "${error_log}"
     cd ../cuTENSORMg/
     error_log=$( nvcc --verbose ${NVCC_FLAGS} --std=c++11 -I${PREFIX}/include -L${PREFIX}/lib -lcutensorMg -lcutensor -lcudart contraction_multi_gpu.cu -o contraction_multi_gpu 2>&1 )
