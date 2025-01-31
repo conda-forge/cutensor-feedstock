3bcc3df0b009fe17e6a41b0d58cdd374b56e3a44
jakirkham <jakirkham@gmail.com>
2025-01-30 16:44:22 -0800

    On Windows, switch to `bld.bat`

---
+++ recipe/bld.bat
@@ -0,0 +1,15 @@
+@echo on
+
+if not exist %PREFIX% mkdir %PREFIX%
+if not exist %LIBRARY_PREFIX% mkdir %LIBRARY_PREFIX%
+if not exist %LIBRARY_BIN% mkdir %LIBRARY_BIN%
+if not exist %LIBRARY_LIB% mkdir %LIBRARY_LIB%
+if not exist %LIBRARY_INC% mkdir %LIBRARY_INC%
+
+copy include\cutensor.h %LIBRARY_INC%\
+copy include\cutensorMg.h %LIBRARY_INC%\
+mkdir %LIBRARY_INC%\cutensor
+copy include\cutensor\types.h %LIBRARY_INC%\cutensor\
+del lib\{{ cuda_major }}\*static*
+copy lib\{{ cuda_major }}\*.dll %LIBRARY_BIN%\
+copy lib\{{ cuda_major }}\*.lib %LIBRARY_LIB%\
