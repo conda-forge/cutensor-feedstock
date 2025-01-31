1ec96e7d826dc9d739cbc8c263a3242a50b4ce0f
jakirkham <jakirkham@gmail.com>
2025-01-30 19:47:40 -0800

    Set `CLEANUP_DIRS` outside `if`
    Something funky is happening with this variable where nothing is
    actually iterated over and removed in the `for`-loop. Try setting this
    outside the `for`-loop in case the indenting is somehow causing syntax
    issues when parsing the list.

--- recipe/run_conda_forge_build_setup_win.bat
+++ recipe/run_conda_forge_build_setup_win.bat
@@ -3,11 +3,10 @@
 :: Hack to free up additional space on Azure
 :: Replace with conda-smithy solution when available
 :: xref: https://github.com/conda-forge/conda-smithy/pull/1966
+set CLEANUP_DIRS=^
+C:\hostedtoolcache\windows;^
+;
 if "%CI%" == "azure" (
-    set CLEANUP_DIRS=^
-    C:\hostedtoolcache\windows;^
-    ;
-
     mkdir C:\empty
     for %%f in (%CLEANUP_DIRS:;= %) do (
         if not [%%f] == [] (
@@ -19,6 +18,7 @@ if "%CI%" == "azure" (
     )
     rmdir /q C:\empty
 )
+set CLEANUP_DIRS=
 
 :: Afterwards run the normal Windows setup steps
 call run_conda_forge_build_setup
