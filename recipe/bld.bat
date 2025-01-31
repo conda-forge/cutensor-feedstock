e932e161a046ff0cc49ea44ec291d383f1dc519c
jakirkham <jakirkham@gmail.com>
2025-01-30 16:47:17 -0800

    Add workaround to free up space on Azure image

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,25 @@
 @echo on
 
+:: Hack to free up additional space on Azure
+:: Replace with conda-smithy solution when available
+:: xref: https://github.com/conda-forge/conda-smithy/pull/1966
+if "%CI%" == "azure" (
+    set CLEANUP_DIRS=^
+    C:\hostedtoolcache\windows;^
+    ;
+
+    mkdir C:\empty
+    for %%f in (%CLEANUP_DIRS:;= %) do (
+        if not [%%f] == [] (
+        echo Removing %%f
+        dir %%f
+        robocopy /mir /mt /zb /ns /nc /nfl /ndl /np /njh /njs C:\empty %%f > nul 2>&1
+        rmdir /q %%f
+        )
+    )
+    rmdir /q C:\empty
+)
+
 if not exist %PREFIX% mkdir %PREFIX%
 if not exist %LIBRARY_PREFIX% mkdir %LIBRARY_PREFIX%
 if not exist %LIBRARY_BIN% mkdir %LIBRARY_BIN%
