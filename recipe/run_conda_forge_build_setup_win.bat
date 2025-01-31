e67026a90db27956e798d6adff6ce189190c49e8
jakirkham <jakirkham@gmail.com>
2025-01-30 19:18:53 -0800

    Move Azure Windows cleanup into CI setup step
    This cleanup needs to happen before the `redist` is downloaded.
    Otherwise we run out of space when downloading or uncompressing it. So
    use a custom CI setup script to do this cleanup during CI setup before
    the recipe build has started.

---
+++ recipe/run_conda_forge_build_setup_win.bat
@@ -0,0 +1,24 @@
+@echo on
+
+:: Hack to free up additional space on Azure
+:: Replace with conda-smithy solution when available
+:: xref: https://github.com/conda-forge/conda-smithy/pull/1966
+if "%CI%" == "azure" (
+    set CLEANUP_DIRS=^
+    C:\hostedtoolcache\windows;^
+    ;
+
+    mkdir C:\empty
+    for %%f in (%CLEANUP_DIRS:;= %) do (
+        if not [%%f] == [] (
+        echo Removing %%f
+        dir %%f
+        robocopy /mir /mt /zb /ns /nc /nfl /ndl /np /njh /njs C:\empty %%f > nul 2>&1
+        rmdir /q %%f
+        )
+    )
+    rmdir /q C:\empty
+)
+
+:: Afterwards run the normal Windows setup steps
+call run_conda_forge_build_setup
