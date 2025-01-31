09ed4af545bd1c8a665acae2000740c7eb26bdde
jakirkham <jakirkham@gmail.com>
2025-01-30 20:06:17 -0800

    Add `errorlevel` checks in `bld.bat`

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,15 +1,27 @@
 @echo on
 
 if not exist %PREFIX% mkdir %PREFIX%
+if errorlevel 1 exit 1
 if not exist %LIBRARY_PREFIX% mkdir %LIBRARY_PREFIX%
+if errorlevel 1 exit 1
 if not exist %LIBRARY_BIN% mkdir %LIBRARY_BIN%
+if errorlevel 1 exit 1
 if not exist %LIBRARY_LIB% mkdir %LIBRARY_LIB%
+if errorlevel 1 exit 1
 if not exist %LIBRARY_INC% mkdir %LIBRARY_INC%
+if errorlevel 1 exit 1
 
 copy include\cutensor.h %LIBRARY_INC%\
+if errorlevel 1 exit 1
 copy include\cutensorMg.h %LIBRARY_INC%\
+if errorlevel 1 exit 1
 mkdir %LIBRARY_INC%\cutensor
+if errorlevel 1 exit 1
 copy include\cutensor\types.h %LIBRARY_INC%\cutensor\
+if errorlevel 1 exit 1
 del lib\%cuda_major%\*static*
+if errorlevel 1 exit 1
 copy lib\%cuda_major%\*.dll %LIBRARY_BIN%\
+if errorlevel 1 exit 1
 copy lib\%cuda_major%\*.lib %LIBRARY_LIB%\
+if errorlevel 1 exit 1
