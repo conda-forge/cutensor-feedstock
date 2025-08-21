098c523340e95df2dadc4d7b5d26eb500cd12eb4
ChrisPsa <15131158+ChrisPsa@users.noreply.github.com>
2025-08-21 12:45:53 +0200

    Fix location of libs

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-check-glibc lib/$cuda_major/*.so*
+check-glibc lib/*.so*
 
 mkdir -p $PREFIX/include
 mv include/* $PREFIX/include/
 mkdir -p $PREFIX/lib
-mv lib/$cuda_major/*.so* $PREFIX/lib/
+mv lib/*.so* $PREFIX/lib/
